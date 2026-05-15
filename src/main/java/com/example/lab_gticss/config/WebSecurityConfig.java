9ac741d5f046ea06d5dbf1ad5af0a4d6c2067943
“Gabriel <sgabriellinares@gmail.com>
2026-05-14 20:39:58 -0500

    ya

--- src/main/java/com/example/lab_gticss/config/WebSecurityConfig.java
+++ src/main/java/com/example/lab_gticss/config/WebSecurityConfig.java
@@ -56,6 +56,7 @@ public class WebSecurityConfig<HttpSecurity> {
                     }
                 });
 
+        http.logout();
 
         http.authorizeHttpRequests()
                 .requestMatchers("/user","/user/**").hasAnyAuthority("admin","user")
