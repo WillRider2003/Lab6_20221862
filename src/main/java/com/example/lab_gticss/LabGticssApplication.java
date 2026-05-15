047b41378624253375e760d88a516dfcc9a88f7f
“Gabriel <sgabriellinares@gmail.com>
2026-05-14 19:18:17 -0500

    commit inicial

---
+++ src/main/java/com/example/lab_gticss/LabGticssApplication.java
@@ -0,0 +1,13 @@
+package com.example.lab_gticss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LabGticssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LabGticssApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/lab_gticss/LabGticssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lab_gticss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LabGticssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
